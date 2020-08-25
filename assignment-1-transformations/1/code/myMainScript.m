31ecc6b6090b7fa340272993b20b2c9dc9b7c399
Meeta 14 <meetamalviya14@gmail.com>
2020-08-25 23:27:43 +0530

    add 1c

--- assignment-1-transformations/1/code/myMainScript.m
+++ assignment-1-transformations/1/code/myMainScript.m
@@ -1,10 +1,11 @@
 %% MyMainScript
 addpath('myShrinkImageByFactorD.m');
+addpath('myNearestNeighborInterpolation.m');
 tic;
 %% Your code here
 
-% circ_conc = imread('circles_concentric.png')
-barbara_small = imread('barbaraSmall.png')
+%circ_conc = imread('circles_concentric.png')
+barbara_small = imread('barbaraSmall.png');
 
 %%%%%%%%%%%%%%%%%%%%%1a%%%%%%%%%%%%%%%%%%
 % subplot(1,3,1), imshow(circ_conc)
@@ -12,7 +13,6 @@ barbara_small = imread('barbaraSmall.png')
 % colorbar
 % title('Original Image')
 % 
-% colorbar
 % scale_2 = myShrinkImageByFactorD(circ_conc, 2)
 % subplot(1,3,2), imshow(scale_2)
 % axis on
@@ -26,7 +26,17 @@ barbara_small = imread('barbaraSmall.png')
 % title('scale by 3')
 
 %%%%%%%%%%%%%%%%%%%%%1c%%%%%%%%%%%%%%%%%%
+final_image = myNearestNeighbor(barbara_small, 3, 2) 
 
+subplot(1,2,1), imshow(barbara_small);
+axis on;
+colorbar;
+title('Original Image');
 
+subplot(1,2,2), imshow(final_image, [0,255]);
+axis on;
+colorbar;
+title('nearest neighbour');
 
 toc;
+
