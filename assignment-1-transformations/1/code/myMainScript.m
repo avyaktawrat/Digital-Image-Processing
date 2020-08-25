0d99d3ba0e9206026b87e996844029f0c6c7819c
Meeta 14 <meetamalviya14@gmail.com>
2020-08-25 18:23:47 +0530

    adding 1a

--- assignment-1-transformations/1/code/myMainScript.m
+++ assignment-1-transformations/1/code/myMainScript.m
@@ -1,7 +1,32 @@
 %% MyMainScript
-
+addpath('myShrinkImageByFactorD.m');
 tic;
+%% Your code here
 
-img = imread('../data/circles_concentric.png')
+% circ_conc = imread('circles_concentric.png')
+barbara_small = imread('barbaraSmall.png')
+
+%%%%%%%%%%%%%%%%%%%%%1a%%%%%%%%%%%%%%%%%%
+% subplot(1,3,1), imshow(circ_conc)
+% axis on
+% colorbar
+% title('Original Image')
+% 
+% colorbar
+% scale_2 = myShrinkImageByFactorD(circ_conc, 2)
+% subplot(1,3,2), imshow(scale_2)
+% axis on
+% colorbar
+% title('Scale by 2')
+% 
+% scale_3 = myShrinkImageByFactorD(circ_conc, 3)
+% subplot(1,3,3), imshow(scale_3)
+% axis on
+% colorbar
+% title('scale by 3')
+
+%%%%%%%%%%%%%%%%%%%%%1c%%%%%%%%%%%%%%%%%%
+
+
 
 toc;
--- assignment-1-transformations/1/code/myShrinkImageByFactorD.m
+++ assignment-1-transformations/1/code/myShrinkImageByFactorD.m
@@ -1,2 +1,3 @@
-
-
+function final_image = myShrinkImageByFactorD(img, d)
+    final_image = img(1:d:end, 1:d:end)
+end
